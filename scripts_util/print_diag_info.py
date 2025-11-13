903bb6d4b585a2546a49626d8b51f35652d1fdad
liaofan.lin <liaofan.lin@noaa.gov>
2025-11-13 23:02:06 +0000

    print diag info script

---
+++ scripts_util/print_diag_info.py
@@ -0,0 +1,14 @@
+from netCDF4 import Dataset
+
+# -- change this --
+#diag_file = "/scratch4/BMC/wrfruc/llin/2025-zrtrr2/240601_misc/20241213-ncdiag-rrfs-full/rrfs/na/prod/rrfs.20240927/06/diag_conv_t_anl.2024092706.nc4"   
+diag_file = "/scratch4/BMC/wrfruc/llin/2025-zrtrr2/240601_misc/20241213-ncdiag-rrfs-full/rrfs/na/prod/rrfs.20240927/06/diag_conv_fed_anl.2024092706.nc4"   
+
+with Dataset(diag_file, 'r') as nc:
+    print("Variables in file:")
+    for v in nc.variables:
+        print(v)
+
+    print("\nGlobal attributes:")
+    for attr in nc.ncattrs():
+        print(attr, "=", getattr(nc, attr))
